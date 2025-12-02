c483a8afe902b954c810dd87666b82f098478acf
richarddiehlrs <77477808+richarddiehlrs@users.noreply.github.com>
2025-12-02 12:52:35 -0300

    Remove GTM script from Header component
    Removed Google Tag Manager script from RootLayout component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -159,38 +159,6 @@ export default function Header() {
           </nav>
         )}
       </div>
-import Script from "next/script";
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt">
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KRNZVXBM');
-          `}
-        </Script>
-      </head>
-      <body>
-        {children}
-
-        {/* GTM NoScript (deve estar logo após o <body>) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KRNZVXBM"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-      </body>
-    </html>
-  );
-}
 
     </header>
   )
