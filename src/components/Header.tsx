ca78f94c235128594f3753bd79c2115c083c3ad7
richarddiehlrs <77477808+richarddiehlrs@users.noreply.github.com>
2025-12-02 12:22:48 -0300

    Fix Google Tag Manager script syntax

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -163,7 +163,7 @@ export default function Header() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KRNZVXBM');</script>
+})(window,document,'script','dataLayer','GTM-KRNZVXBM')</script>
     </header>
   )
 }
